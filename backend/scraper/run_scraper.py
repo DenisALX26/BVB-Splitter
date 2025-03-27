7bad597959734036a1068c2fdcd31584d101cb05
Neacsa Denis <neacsa.denis@yahoo.com>
2025-03-27 15:38:20 +0200

    implemented headless mode + json file

--- backend/scraper/run_scraper.py
+++ backend/scraper/run_scraper.py
@@ -1,12 +1,17 @@
 from bvb.bvb import BVB
-import pprint
+import json
+import os
 
 with BVB() as bot:
     # Parse the BVB page
     bot.land_first_page()
+    bot.pass_pop_up()
     bot.accept_cookies()
     bot.go_to_index_page()
 
     # Get all companies
     list = bot.show_all_companies()
-    pprint.pprint(list)
+
+    # Create the json file
+    with open("companies.json", "w") as file:
+        json.dump(list, file)
