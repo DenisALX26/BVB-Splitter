f0a1fbff4c11001f30bdb2d929082421b8eec61a
Neacsa Denis <neacsa.denis@yahoo.com>
2025-03-28 18:17:29 +0200

    adding path to the system path

--- backend/scraper/run_scraper.py
+++ backend/scraper/run_scraper.py
@@ -1,16 +1,23 @@
-from bvb.bvb import BVB
+import sys, os
+
+# Add the path to the system path
+# This is necessary to import the Bvb class from the bvb module
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+
+from bvb.bvb import Bvb
 from json_utils import save_to_json
 
 
-with BVB() as bot:
-    # Parse the BVB page
-    bot.land_first_page()
-    bot.pass_pop_up()
-    bot.accept_cookies()
-    bot.go_to_index_page()
+def run_scraper():
+    with Bvb() as bot:
+        # Parse the BVB page
+        bot.land_first_page()
+        bot.pass_pop_up()
+        bot.accept_cookies()
+        bot.go_to_index_page()
 
-    # Get all companies
-    list = bot.show_all_companies()
+        # Get all companies
+        list = bot.show_all_companies()
 
-    # Save the data to the json file
-    save_to_json(list)
+        # Save the data to the json file
+        save_to_json(list)
