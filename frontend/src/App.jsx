695451cb5c2ff1c4e94477894b542c49fec5811f
Neacsa Denis <neacsa.denis@yahoo.com>
2025-04-03 14:58:37 +0300

    added "How It Works" section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Calculator from "./sections/Calculator";
+import HowItWorks from "./sections/HowItWorks";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col min-h-screen items-center gap-[96px]">
+      <div className="flex flex-col min-h-screen items-center gap-[96px] bg-white max-w-[1440px]">
         <Header />
         <main className="px-[80px] w-100% flex flex-col gap-[96px] items-center">
           <Hero />
           <Calculator />
+          <HowItWorks />
         </main>
       </div>
     </>
