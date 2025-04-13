73a5be318b14dc017d440465f5cbb3a512b7ba89
Neacsa Denis <neacsa.denis@yahoo.com>
2025-04-14 00:19:07 +0300

    feature(endpoint): implement the 'show distribution' function based on user's amount

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter
 from scraper.json_utils import load_from_json
+import json
+from pathlib import Path
+from services.calculator import calculate_distribution
+from models.company import InvestmentAmount, Company
 
 router = APIRouter()
+
 
 @router.get("/")
 def get_companies():
     return load_from_json()
+
+
+@router.post("/results", response_model=dict[str, dict])
+def show_distribution(request: InvestmentAmount):
+    companies_path = Path(__file__).parent.parent / "data" / "companies.json"
+    with open(companies_path, "r") as file:
+        companies = json.load(file)
+    result = calculate_distribution(companies, request.amount)
+    return result
