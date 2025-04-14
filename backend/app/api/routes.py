16adbf8106a85895c69ffb5dd02300be25d78333
Neacsa Denis <neacsa.denis@yahoo.com>
2025-04-14 21:53:02 +0300

    feat(api): add endpoint for calculating and returning the minimum required investment

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -3,6 +3,7 @@ from scraper.json_utils import load_from_json
 import json
 from pathlib import Path
 from services.calculator import calculate_distribution
+from services.minimum import calculate_minimum_amount
 from models.company import InvestmentAmount, Company
 
 router = APIRouter()
@@ -19,4 +20,13 @@ def show_distribution(request: InvestmentAmount):
     with open(companies_path, "r") as file:
         companies = json.load(file)
     result = calculate_distribution(companies, request.amount)
+    return result
+
+
+@router.get("/minimum", response_model=int)
+def show_minimum():
+    companies_path = Path(__file__).parent.parent / "data" / "companies.json"
+    with open(companies_path, "r") as file:
+        companies = json.load(file)
+    result = calculate_minimum_amount(companies)
     return result
